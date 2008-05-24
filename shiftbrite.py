6dda1205d1f7bbe3eee2e24f4b0e55d02a478a5f
drewp <drewp@bigasterisk.com>
2008-05-24 00:39:45 -0700

    shiftbrite.py now has tk, xmlrpc, and module modes
    darcs-hash:20080524073945-312f9-6d8afe95f13cc7f1d57381702c238d3818051b02.gz

--- shiftbrite.py
+++ shiftbrite.py
@@ -1,34 +1,86 @@
 #!/usr/bin/python
+"""
+launch with no args for GUI
+
+launch with "-port xxxx" to serve xmlrpc
+
+import from python to use ShiftBriteOutput class
+"""
+
 from __future__ import division
-import time
+import sys
 import getserial
 
-ser = getserial.getSerial(115200)
+class ShiftBriteOutput(object):
+    def __init__(self):
+        self.ser = getserial.getSerial(115200)
+        self.profile = None
 
-## delay = .1
-## while True:
-##     for i in range(5):
-##         writeColor(150, 50, 0)
-##     time.sleep(delay)
-    
-##     for i in range(5):
-##         writeColor(0, 0, 200)
-##     time.sleep(delay)    
+    def setProfile(self, name):
+        """apply some named transfer function on levels before they're
+        output"""
+        self.profile = getattr(Profile, name)
 
-import Tkinter as tk
-root = tk.Tk()
-chans = dict(r=tk.IntVar(), g=tk.IntVar(), b=tk.IntVar())
-def update():
-    msg = ""
-    for i in range(5):
-        msg += chr(chans['r'].get()) + chr(chans['g'].get()) + chr(chans['b'].get())
-    ser.write(msg)
-    print "write"
-                   
-for chan in 'rgb':
-    s = tk.Scale(root, label=chan,from_=255, to=0, showval=1,
-                 command=lambda v,chan=chan: (chans[chan].set(v),update()),
-                 length=300,width=80,sliderlen=80)
-    s.pack(side='left',fill='both',exp=1)
+    def send(self, levels):
+        """list of (r,g,b) values for all lights"""
+        msg = ""
+        for r, g, b in levels:
+            if self.profile is not None:
+                r, g, b = self.profile(r, g, b)
+            msg += chr(r) + chr(g) + chr(b)
+        self.ser.write("\xff" + msg.replace("\xff", "\xfe"))
 
-tk.mainloop()
+class Profile(object):
+    @staticmethod
+    def eyeball(r, g, b):
+        """I guessed a whitepoint"""
+        return r, int(g * 183 / 255), int(b * 45 / 255)
+    # gamma up
+    # something that's mostly accurate, but lets blues go really bright
+
+def xmlrpcServer(port=9002):
+    """expose the ShiftBriteOutput.send method via xmlrpc. Example
+    client:
+
+    import xmlrpclib
+    serv = xmlrpclib.Server('http://localhost:9002/')
+    g = (0, 254, 0)
+    serv.send([g, g, g, g, g])
+    """
+    from twisted.internet import reactor
+    from twisted.web import xmlrpc, server
+    from twisted.python import log
+    log.startLogging(sys.stdout)
+    out = ShiftBriteOutput()
+    class XMLRPCServe(xmlrpc.XMLRPC):
+        def xmlrpc_send(self, levels):
+            out.send(levels)
+            return "ok"
+    reactor.listenTCP(port, server.Site(XMLRPCServe()))
+    reactor.run()
+
+def tkSliders(nleds=5):
+    """Present r/g/b sliders that set the color of all leds. runs a tk
+    mainloop"""
+    import Tkinter as tk
+    root = tk.Tk()
+    chans = dict(r=tk.IntVar(), g=tk.IntVar(), b=tk.IntVar())
+
+    out = ShiftBriteOutput()
+    def update():
+        out.send([(chans['r'].get(), chans['g'].get(), chans['b'].get())
+                  for loop in range(nleds)])
+
+    for chan in 'rgb':
+        s = tk.Scale(root, label=chan,from_=254, to=0, showval=1,
+                     command=lambda v,chan=chan: (chans[chan].set(v),update()),
+                     length=300,width=80,sliderlen=80)
+        s.pack(side='left',fill='both',exp=1)
+
+    tk.mainloop()
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3 and sys.argv[1] == '-port':
+        xmlrpcServer(port=int(sys.argv[2]))
+    else:
+        tkSliders()
