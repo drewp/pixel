f8e9f167793f8082ec45868424cc00fe8d639194
drewp <drewp@bigasterisk.com>
2011-03-26 16:54:47 -0700

    nightlight now listens for OSC messages
    Ignore-this: 2217001d2b8117d418f7384dbff0ff6e
    
    darcs-hash:20110326235447-312f9-640aa8235842a4235b0856a31f6cbfa39b35bcf4.gz

--- nightlight.py
+++ nightlight.py
@@ -1,5 +1,9 @@
 from __future__ import division
-import time, math, sys, Image, datetime, os, logging
+import time, math, sys, Image, os, logging
+from datetime import datetime, timedelta
+from twisted.internet import reactor, task
+from txosc import osc, dispatch, async
+
 sys.path.append("shiftweb")
 from multiclient import setColor
 logging.basicConfig()
@@ -25,22 +29,55 @@ class Img(object):
             self.reread()
         return [v * 4 for v in self.img.getpixel((x, y))[:3]]
         
+class ReceiverApplication(object):
+    """
+    receive UDP OSC messages for light color settings
+    """
+    def __init__(self, port, lightState):
+        self.port = port
+        self.lightState = lightState
+        self.receiver = dispatch.Receiver()
+        self.receiver.addCallback("/pixel/*", self.pixel_handler)
+        self._server_port = reactor.listenUDP(self.port, async.DatagramServerProtocol(self.receiver), interface='0.0.0.0')
+        print("Listening on udp port %s" % (self.port))
 
-def main():
+    def pixel_handler(self, message, address):
+        light = message.address.split('/')[2]
+        rgb = [a.value * 1023 for a in message.arguments]
+        print "OSC: set %s to %s" % (light, rgb)
+        setColor(light, rgb)
+        self.lightState.mute(light, 3)
 
-    img = Img("nightlight.png")
-    while 1:
-        now = datetime.datetime.now()
+lightYPos = {
+    'deskLeft' : 125,
+    'deskRight' : 175,
+    'bathroom' : 75,
+    'ari1' : 225,
+    'ari2' : 275,
+    'ari3' : 325,
+}
+
+class LightState(object):
+    def __init__(self):
+        self.img = Img("nightlight.png")
+        self.autosetAfter = dict.fromkeys(lightYPos.keys(),
+                                          datetime.fromtimestamp(0))
+
+    def mute(self, name, secs):
+        """don't autoset this light for a few seconds"""
+        self.autosetAfter[name] = datetime.now() + timedelta(seconds=secs)
+
+    def step(self):
+        now = datetime.now()
         hr = now.hour + now.minute / 60 + now.second / 3600
         x = int(((hr - 12) % 24) * 50)
         log.info("x = %s", x)
 
-        setColor('deskLeft', img.getColor(x, 125))
-        setColor('deskRight', img.getColor(x, 175))
-        setColor('bathroom', img.getColor(x, 75))
-        setColor('ari1', img.getColor(x, 225))
-        setColor('ari2', img.getColor(x, 275))
-        setColor('ari3', img.getColor(x, 325))
+        for name, ypos in lightYPos.items():
+            if now > self.autosetAfter[name]:
+                setColor(name, self.img.getColor(x, ypos))
 
-        time.sleep(3)
-main()
+lightState = LightState()
+task.LoopingCall(lightState.step).start(1)
+app = ReceiverApplication(9050, lightState)
+reactor.run()
